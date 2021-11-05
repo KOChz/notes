a5b86bedd4502d912108c6626922ce1e767e696f
KOchz <kirill1chernomazov@gmail.com>
2021-11-05 18:57:05 +0200

    solve issues in last PR and delete noneed stuf and redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,1 @@
-import { applyMiddleware, createStore } from 'redux'
-import thunk from 'redux-thunk'
-import logger from 'redux-logger'
-import reducers from './reducers/reducers'
 
-const store = createStore(reducers, applyMiddleware(thunk, logger))
-
-export default store
